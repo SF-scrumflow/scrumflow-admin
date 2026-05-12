230a5cad86ed5ab6233f86914f473fc7c7080a51
GMatiasAlwaysfit <gabriel@alwaysfit.com.br>
2026-05-12 10:20:27 -0300

    fixing dashboardcontroller

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,7 +2,7 @@ class Admin::DashboardController < Admin::BaseController
   def index
     Rails.logger.info "ANTES"
 
-    Enterprise.connection.execute("SELECT 1")
+    Enterprise.connection.execute("SELECT 1") 
 
     Rails.logger.info "DEPOIS"
   end
