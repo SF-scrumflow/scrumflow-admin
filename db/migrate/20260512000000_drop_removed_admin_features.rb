886d5736380c2f2d6a563837be8f5054b7ca5dd9
TKON-br <biel.augustinho@live.com>
2026-05-16 15:19:46 -0300

    Reverte remoção de foreign key e limpa testes/fixtures de recursos removidos

--- db/migrate/20260512000000_drop_removed_admin_features.rb
+++ db/migrate/20260512000000_drop_removed_admin_features.rb
@@ -1,7 +1,5 @@
 class DropRemovedAdminFeatures < ActiveRecord::Migration[7.2]
   def change
-    remove_foreign_key :payments, :subscriptions if foreign_key_exists?(:payments, :subscriptions)
-
     drop_table :subscriptions, if_exists: true
 
     drop_table :enterprises, if_exists: true
